f2b4f72afdf90f4f6441010aa09c1f322f49af0f
shegute <ny812002@yahoo.com>
2022-08-15 01:57:12 -0400

    - add an event emitter to when the toolbar button is clicked and output it to the sidebar so that it opens or collapses - add a breakpoint observer in the sidebar to detect when the screen size is small, do not show the sidebar by default, by setting a variable - define the opened and mode property for the sidebar. Set opened to true if screen size is not small, - define the mode property for the sidebar. Set the sidebar opened mode as 'over', over the main content if the screen is small and 'side' as side by side with the main content.

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,5 +1,7 @@
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
+const SMALL_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
@@ -7,11 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  public isScreenSmall: boolean;
+  constructor(private breakpointObserver:BreakpointObserver) {
+
+   }
 
   ngOnInit(): void {
+    this.breakpointObserver
+    //.observe([Breakpoints.XSmall]) //predefined breakpoint size
+    .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`]) //using our own self-defined screen width to hit breakpoint
+    .subscribe((state:BreakpointState) => {
+      this.isScreenSmall = state.matches;
+    })
   }
 
-
-  toggle() {}
 }
